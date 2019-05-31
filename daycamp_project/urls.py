875d9101bc0e192d2d48f8b090ca2981539e6c84
Teresa Cho <tcho@cca.edu>
2019-05-31 14:30:42 -0700

    Adds registration and accounts folder

--- daycamp_project/urls.py
+++ daycamp_project/urls.py
@@ -19,6 +19,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', views.register, name='register')
-
+    path('register/', views.register, name='register'),
+    # path('login/', views.login, name='login'),
+    # path('logout/', views.logout, name='logout'),
 ]
