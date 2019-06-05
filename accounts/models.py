13eb4238f8abac712fc6ea8fa044fc14ff4b4410
Teresa Cho <tcho@cca.edu>
2019-06-04 17:01:30 -0700

    changed the vet_info model to text-field

--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model) :
   user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+  name = models.CharField(max_length=100, null=True, blank=True)
   profile_picture = models.CharField(max_length=100, null=True, blank=True)
   vet_info = models.TextField()
   contact_info= models.CharField(max_length=100, null=True, blank=True)
